6b9147861493c9fdc56b675c3547b5084ef955f9
frstlis <48125063@qq.com>
2021-10-07 20:54:36 -0700

    20211008-1

---
+++ helloworld/target/debug/deps/helloworld-9a5a60bb31b91773.d
@@ -0,0 +1,5 @@
+/home/noi/geek/gitproject/Rust.frstlis.ssh/helloworld/target/debug/deps/helloworld-9a5a60bb31b91773: src/main.rs
+
+/home/noi/geek/gitproject/Rust.frstlis.ssh/helloworld/target/debug/deps/helloworld-9a5a60bb31b91773.d: src/main.rs
+
+src/main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/home/noi/geek/gitproject/Rust.frstlis.ssh/helloworld/target/debug/helloworld: /home/noi/geek/gitproject/Rust.frstlis.ssh/helloworld/src/main.rs
